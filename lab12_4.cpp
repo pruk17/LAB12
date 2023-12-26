de7ce3c1c2420ef43b4c17629c58613208b69db1
Chaiyapruk Parinyanuntakan <chaiyapruk6494@gmail.com>
2023-12-26 16:11:50 +0700

    add lab4_4

--- lab12_4.cpp
+++ lab12_4.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+template <class para>
+void mySwap(para &, para &);
+
 int main(){
 	int x, y;
 	string a, b;
@@ -30,3 +33,11 @@ int main(){
 	return 0;
 }
 
+template <class para>
+void mySwap(para &x, para &y){
+	para A = x;
+	para B = y;
+	y = A;
+	x = B;
+
+}
