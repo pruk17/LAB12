51fe28acbe9577100678966e739ad5d65921aa1a
Chaiyapruk Parinyanuntakan <chaiyapruk6494@gmail.com>
2023-12-26 15:31:45 +0700

    add lab12_1

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,29 @@
 #include<iostream>
+#include <ctime>
 using namespace std;
 
+long long int fibonacci(long long int);
+
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
-    return 0; 
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
+}
+
+long long int fibonacci(long long int y = 0){
+    if (y <= 0)
+    {
+        return y;
+    }
+    if ( y == 1)
+    {
+        return y;
+    }
+    else
+    {
+        return fibonacci(y-1) + fibonacci(y-2);
+    }
 }
